c089245cef6b3c7632618b9d1218ffde42bb8d3f
Himanshu <himanshuchaudhary586@gmail.com>
2026-06-27 02:56:36 +0530

    f

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ app.get("/home",(req,res)=>{
     res.send("welcome to home page");
 }) */
 app.post("/home",(req,res)=>{
-    res.send("post request to home page");
+    res.send("post request to home page.");
 })
 app.listen(3000,()=>{
     console.log("server is running on port 3000");  
