7cb01cd364a7c1d2f675997992fdd0a730049342
Himanshu <himanshuchaudhary586@gmail.com>
2026-06-30 17:50:13 +0530

    small edit

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const mongoose=require('mongoose');
 
 const database=async ()=>{
-    await mongoose.connect("mongodb+srv://himanshuchaudhary586:hc4mongodb@cluster0.5vhyl.mongodb.net/Devtinder?appName=Devtinder");
+    await mongoose.connect(`mongodb+srv://himanshuchaudhary586:${encodeURIComponent(process.env.PASSWORD)}@cluster0.5vhyl.mongodb.net/Devtinder?appName=Devtinder`);
 };
 
 module.exports=database;
