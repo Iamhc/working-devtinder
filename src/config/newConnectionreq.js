e8be1184864643eabdb88e9742a59dcc5d6585c3
Himanshu <himanshuchaudhary586@gmail.com>
2026-08-26 18:28:49 +0530

    connection req

--- src/config/newConnectionreq.js
+++ src/config/newConnectionreq.js
@@ -1,15 +1,15 @@
 const mongoose=require("mongoose");
 const connectionSchema=new mongoose.Schema({
     fromUserId:{
-        types:mongoose.Schema.Types.ObjectId,
+        type:mongoose.Schema.Types.ObjectId,
         required:true
     },
     toUserId:{
-        types:mongoose.Schema.Types.ObjectId,
+        type:mongoose.Schema.Types.ObjectId,
         required:true
     },
     status:{
-        types:String,
+        type:String,
         enum:{
             values:["INTERESTED","REJECTED","ACCEPTED","NOT INTERESTED"],
             message:`{values} not in values`
--- src/routes/request.js
+++ src/routes/request.js
@@ -1,11 +1,27 @@
 const express=require("express")
 const router=express.Router();
 const connectionRequest=require("../config/newConnectionreq");
+const authRouter = require("./auth");
+const middleware=require("./middleware");
 
-router.post("/request/:status/:toUserId",
- new connectionRequest({
-    fromUserId:req.user._id,
-    toUserId:toUserId,
-    status:status
- }) 
+router.post("/request/:status/:toUserId",authRouter,async (req,res)=>{   
+    try{
+     const toUser=req.user._id;
+    const fromUser=req.params.toUserId;
+    const status=req.params.status;
+    const ConnectionRequest=new connectionRequest({
+    toUser,
+    fromUser,
+    status
+ })
+   await ConnectionRequest.save();
+   res.send("connection done");
+    }
+    catch(err){
+    res.status(400).send(err.message);
+    }
+   
+}
+ 
 )
+module.exports=router;
