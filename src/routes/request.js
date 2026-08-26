137fcbd3f587ef82ececdc3c948b44719335794e
Himanshu <himanshuchaudhary586@gmail.com>
2026-08-26 17:57:09 +0530

    connection req

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,6 +1,11 @@
 const express=require("express")
 const router=express.Router();
+const connectionRequest=require("../config/newConnectionreq");
 
-router.post("/request/:status/:touserid",
-    
+router.post("/request/:status/:toUserId",
+ new connectionRequest({
+    fromUserId:req.user._id,
+    toUserId:toUserId,
+    status:status
+ }) 
 )
